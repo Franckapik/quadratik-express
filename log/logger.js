0b7ea148038d4557d1c422f1cf6ac34b4c6f9807
franckapik <fanch44@hotmail.com>
2019-12-26 22:42:16 +0100

    logger change

--- log/logger.js
+++ log/logger.js
@@ -12,14 +12,16 @@ const env = process.env.NODE_ENV;
 
 const logger = winston.createLogger({
   level: env!== 'production' ? 'debug' : 'info', //le niveau affiché au npm start
-  format:
-    winston.format.combine(
-    winston.format.colorize(),
-    winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
-    }),
-    winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-  ),
+  format: winston.format.combine(
+  winston.format.colorize(),
+  winston.format.prettyPrint(),
+  winston.format.splat(),
+  winston.format.simple(),
+  winston.format.printf(
+    info =>
+      `${info.timestamp} ${info.level} : ${info.message}`
+  )
+),
   transports: [
     new winston.transports.File({ filename: 'log/error.log', level: 'error' }),
     new winston.transports.File({ filename: 'log/combined.log' })
